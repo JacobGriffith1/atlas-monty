55a675dff6a8268538a1a20dce9111f8b7f0dc84
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-04-06 03:36:40 +0000

    appended list of headers contained within monty.h

--- monty.h
+++ monty.h
@@ -1,11 +1,7 @@
 #ifndef _MONTY_H_
 #define _MONTY_H_
 
-#include <ctype.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
 
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
