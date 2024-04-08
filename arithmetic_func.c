86f8ba5d87f7ee40ec7fdfeec042243ba292d67e
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-04-08 02:05:12 +0000

    _add opcode implemented

--- arithmetic_func.c
+++ arithmetic_func.c
@@ -6,7 +6,10 @@
  * @line_number: Line number.
  * Return: N/A
  *
- * Description -
+ * Description - "_add" is an opcode that adds the values of the top two
+ * elements on the stack. The value pointed to by "(*stack)->n" (first node)
+ * is added to the value pointed to by "temp->n" (second node), and then the
+ * top node is popped, leaving the appended value at the top of the stack.
  */
 
 void _add(stack_t **stack, unsigned int line_number)
@@ -21,10 +24,14 @@ void _add(stack_t **stack, unsigned int line_number)
 	if (nodect < 2)
 	{
 		fprintf(stderr, "L%d: ", line_number);
-		fprintf(stderr, "");
+		fprintf(stderr, "can't add, stack to short");
 		free_gvar();
 		exit(EXIT_FAILURE);
 	}
+
+	temp = (*stack)->next;
+	temp->n += (*stack)->n;
+	_pop(stack, line_number);
 }
 
 /**
