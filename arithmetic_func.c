a63374e7363acf08b3efb6993fe9c59d2d6ac0ff
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-04-08 01:52:41 +0000

    some basic framework for input checking in arithmetic_func

--- arithmetic_func.c
+++ arithmetic_func.c
@@ -1,46 +1,134 @@
 #include "monty.h"
 
 /**
- * _add -
- * @stack:
- * @line_number:
- * Return:
+ * _add - Adds the top two elements of the stack.
+ * @stack: The stack.
+ * @line_number: Line number.
+ * Return: N/A
  *
  * Description -
  */
+
+void _add(stack_t **stack, unsigned int line_number)
+{
+	int nodect = 0;
+	stack_t *temp = NULL;
+
+	temp = *stack;
+
+	for (; temp != NULL; temp = temp->next, nodect++)
+		;
+	if (nodect < 2)
+	{
+		fprintf(stderr, "L%d: ", line_number);
+		fprintf(stderr, "");
+		free_gvar();
+		exit(EXIT_FAILURE);
+	}
+}
 
 /**
  * _sub -
- * @stack:
- * @line_number:
- * Return:
+ * @stack: The stack.
+ * @line_number: Line number.
+ * Return: N/A
  *
  * Description -
  */
+
+void _sub(stack_t **stack, unsigned int line_number)
+{
+	int nodect = 0;
+	stack_t *temp = NULL;
+
+	temp = *stack;
+
+	for (; temp != NULL; temp = temp->next, nodect++)
+		;
+	if (nodect < 2)
+	{
+		fprintf(stderr, "L%d: ", line_number);
+		fprintf(stderr, "");
+		free_gvar();
+		exit(EXIT_FAILURE);
+	}
+}
 
 /**
  * _mul -
- * @stack:
- * @line_number:
- * Return:
+ * @stack: The stack.
+ * @line_number: Line number.
+ * Return: N/A
  *
  * Description -
  */
 
+void _mul(stack_t **stack, unsigned int line_number)
+{
+	int nodect = 0;
+	stack_t *temp = NULL;
+
+	temp = *stack;
+
+	for (; temp != NULL; temp = temp->next, nodect++)
+		;
+	if (nodect < 2)
+	{
+		fprintf(stderr, "L%d: ", line_number);
+		fprintf(stderr, "");
+		free_gvar();
+		exit(EXIT_FAILURE);
+	}
+}
 /**
  * _div -
- * @stack:
- * @line_number:
- * Return:
+ * @stack: The stack.
+ * @line_number: Line number.
+ * Return: N/A
  *
  * Description -
  */
 
+void _div(stack_t **stack, unsigned int line_number)
+{
+	int nodect = 0;
+	stack_t *temp = NULL;
+
+	temp = *stack;
+
+	for (; temp != NULL; temp = temp->next, nodect++)
+		;
+	if (nodect < 2)
+	{
+		fprintf(stderr, "L%d: ", line_number);
+		fprintf(stderr, "");
+		free_gvar();
+		exit(EXIT_FAILURE);
+	}
+}
 /**
  * _mod -
- * @stack:
- * @line_number:
- * Return:
+ * @stack: The stack.
+ * @line_number: Line number.
+ * Return: N/A
  *
  * Description -
  */
+
+void _mod(stack_t **stack, unsigned int line_number)
+{
+	int nodect = 0;
+	stack_t *temp = NULL;
+
+	temp = *stack;
+
+	for (; temp != NULL; temp = temp->next, nodect++)
+		;
+	if (nodect < 2)
+	{
+		fprintf(stderr, "L%d: ", line_number);
+		fprintf(stderr, "");
+		free_gvar();
+		exit(EXIT_FAILURE);
+	}
+}
